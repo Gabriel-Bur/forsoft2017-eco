fd372d938050ecb584ba932653e4f162bbf06ee6
Gabriel Bur <gabriel.bur@al.infnet.edu.br>
2018-05-18 21:26:42 -0300

    readme

--- eco_solution/DAO/Conexao.cs
+++ eco_solution/DAO/Conexao.cs
@@ -17,11 +17,11 @@ namespace eco_solution.DAO
         public Conexao()
         {
             // CentOS
-             con = new MySqlConnection("server=10.74.0.4;port=3306;database=Ecossistema;uid=forsoft;password=E2A3C8F4;SslMode=none;Allow User Variables=True");
+            //con = new MySqlConnection("server=10.74.0.4;port=3306;database=Ecossistema;uid=forsoft;password=E2A3C8F4;SslMode=none;Allow User Variables=True");
 
 
             //local
-            //con = new MySqlConnection("server=192.168.0.45;port=3306;database=Ecossistema;uid=forsoft;password=f0rs0ft;SslMode=none;Allow User Variables=True");
+            con = new MySqlConnection("server=192.168.0.45;port=3306;database=Ecossistema;uid=forsoft;password=f0rs0ft;SslMode=none;Allow User Variables=True");
         }
 
 
